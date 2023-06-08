f39b72110a050c679bfe608de5717793bbdc93b7
Amit-kharod <kharodcloud1@gmail.com>
2023-06-08 22:28:16 +0530

    add cities API

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import flightPrice from './routes/api/flight-price';
+import cities from './routes/api/cities';
+import connectDB from './config/mongodb';
 
 const app = express();
+
+// Connecting MongoDB
+connectDB();
 
 app.use(express.json());
 
 app.use('/flight-price', flightPrice);
+app.use('/cities', cities);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
