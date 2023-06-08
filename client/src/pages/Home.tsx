638d8e485255eee63bc4e3e2ed19b66d8ebbd7af
Amit-kharod <kharodcloud1@gmail.com>
2023-06-08 18:15:14 +0530

    implement heading component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import { Header } from "../components/Header";
+
 interface ILoginProps {
   isLogin: Boolean;
 }
 
 export const Home: React.FC<ILoginProps> = ({ isLogin }) => {
-  return <div>Home</div>;
+  return <div className="max-w-[80vw] mx-auto">
+    <Header text={"Get go to price of flights in seconds..."} />
+  </div>;
 };
 
 
